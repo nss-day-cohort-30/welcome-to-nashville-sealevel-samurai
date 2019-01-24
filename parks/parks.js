604cc15ae13631ec894ca85f74a253bee3473459
Connor Bailey <bailey.connor.p@gmail.com>
2019-01-24 16:00:57 -0600

    working on dynamic urlSearch

--- parks/parks.js
+++ parks/parks.js
@@ -1,16 +1,25 @@
 const searchStart = "https://data.nashville.gov/resource/xbru-cfzi.json?"
+const APIKey = "&$$app_token=AJNDweW2mpoIQRcy43HbdJgdq"
 
 // function that will read which dropdown options are checked, and create the string of filters that needs to be inserted into the Fetch URL
 let filters = ""
-
 makeURLFilters = function() {
-    document.querySelectorAll()
+    let parkFeatures = document.querySelectorAll(".parkFeature").checked = true;
+    parkFeatures.forEach(feature => {
+        console.log(feature)
+        
+    });
+
 }
 
-let searchReady = searchStart.concat(filters)
+
+document.querySelector("searchButton").addEventListener("click", makeURLFilters)
+
+// add all search strings together
+let searchReady = searchStart.concat(filters).concat(APIKey)
 
 // fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=AJNDweW2mpoIQRcy43HbdJgdq")
-fetch(`${searchReady}&$$app_token=AJNDweW2mpoIQRcy43HbdJgdq`)
+fetch(`${searchReady}`)
     .then(parks => parks.json())
     .then(parsedParks => {
         parsedParks.forEach(park =>
