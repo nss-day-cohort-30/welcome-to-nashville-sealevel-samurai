0d51745117ed242f3a075bf66974f45f40681561
nathan sepulveda <nathans.composer@gmail.com>
2019-01-25 09:52:51 -0600

    updated serach method

--- restaurants/restaurans.js
+++ restaurants/restaurans.js
@@ -3,7 +3,11 @@ let arrayOfResturants = [
 
 ]
 
-fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076
+foodType = prompt("What are you looking for?")
+
+
+fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}
+
 `)
     .then(response => response.json())
     .then(restInfo => {
@@ -12,19 +16,22 @@ fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=
             let name = restInfo.restaurants[i].restaurant.name;
             let type = restInfo.restaurants[i].restaurant.cuisines;
             let cost = restInfo.restaurants[i].restaurant.average_cost_for_two;
+            let location = restInfo.restaurants[i].restaurant.location.locality
             let blankRestObject = {}
 
             blankRestObject.names = name
             blankRestObject.types = type
             blankRestObject.costs = cost
+            blankRestObject.locations = location
 
             arrayOfResturants.push(blankRestObject)
-
         }
-
         console.table(arrayOfResturants)
-
+        if (arrayOfResturants.length < 3) {
+            alert("Please try another query.")
+        }
     })
+
 
 
 
