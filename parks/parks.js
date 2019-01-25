58bc4764dfcba765068a5e4c9c664d5f3c3a4607
Connor Bailey <bailey.connor.p@gmail.com>
2019-01-25 10:18:09 -0600

    search resets results every time to prevent the results list from building with duplicates during every search

--- parks/parks.js
+++ parks/parks.js
@@ -4,7 +4,7 @@ let filters = ""
 
 
 // function that will read which dropdown options are checked, and create the string of filters that needs to be inserted into the Fetch URL
-makeURLFilters = function () {
+searchParksAPI = function () {
     let URLFilterString = [];
     let parkFeatures = document.querySelectorAll(".parkFeature")
     parkFeatures.forEach(feature => {
@@ -14,15 +14,30 @@ makeURLFilters = function () {
     });
     filters = URLFilterString.join("&");
     // add all search strings together
-    let searchReady = searchStart.concat(filters).concat(APIKey)
-    fetch(`${searchReady}`)
+    let fullSearchURL = searchStart.concat(filters).concat(APIKey)
+    fetch(`${fullSearchURL}`)
         .then(parks => parks.json())
         .then(parsedParks => {
+            let parksThatMatch = document.querySelector("#parksThatMatch")
+            if (parksThatMatch.hasChildNodes()) {
+                parksThatMatch.innerHTML = "";
+            }
             if (parsedParks.length === 0) {
-                console.log(`No parks match your criteria, please try a less restrictive search!S`)
+                parksThatMatch.innerHTML  = `
+                <div id="noParks" class="noParks">
+                    No parks match your criteria, please try a less restrictive search!
+                </div>
+                `
             } else {
-            parsedParks.forEach(park => {
-                    console.log(park)
+                parsedParks.forEach(park => {
+                    let parkName = park.park_name;
+                    let parkAddress = `${park.mapped_location_address}, ${park.mapped_location_city}, ${park.mapped_location_state}, ${park.mapped_location_zip}`;
+                    parksThatMatch.innerHTML += `
+                        <article id="${parkName}" class="matchingPark">
+                            <h2 id="${parkName}-header" class="matchingParkHeader">${parkName}</h2>
+                            <div id="${parkName}-address" class="matchingParkAddress">${parkAddress}</div>
+                        </article>
+                    `
                 })
             }
         })
@@ -31,7 +46,7 @@ makeURLFilters = function () {
 
 
 
-document.querySelector("#searchButton").addEventListener("click", makeURLFilters)
+document.querySelector("#searchButton").addEventListener("click", searchParksAPI)
 
 
 // fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=AJNDweW2mpoIQRcy43HbdJgdq")
