8dd657c8f1926bb67da6c01d92c0363ac6098997
Connor Bailey <bailey.connor.p@gmail.com>
2019-01-25 09:50:10 -0600

    fetch works off of selected checkboxes, also returns a try again message if zero parks are returned

--- parks/parks.js
+++ parks/parks.js
@@ -1,22 +1,38 @@
 const searchStart = "https://data.nashville.gov/resource/xbru-cfzi.json?"
 const APIKey = "&$$app_token=AJNDweW2mpoIQRcy43HbdJgdq"
+let filters = ""
+
 
 // function that will read which dropdown options are checked, and create the string of filters that needs to be inserted into the Fetch URL
-let filters = ""
 makeURLFilters = function () {
+    let URLFilterString = [];
     let parkFeatures = document.querySelectorAll(".parkFeature")
     parkFeatures.forEach(feature => {
         if (feature.checked === true) {
+            URLFilterString.push(`${feature.id}=Yes`)
         }
-        
     });
+    filters = URLFilterString.join("&");
+    // add all search strings together
+    let searchReady = searchStart.concat(filters).concat(APIKey)
+    fetch(`${searchReady}`)
+        .then(parks => parks.json())
+        .then(parsedParks => {
+            if (parsedParks.length === 0) {
+                console.log(`No parks match your criteria, please try a less restrictive search!S`)
+            } else {
+            parsedParks.forEach(park => {
+                    console.log(park)
+                })
+            }
+        })
 }
+
+
 
 
 document.querySelector("#searchButton").addEventListener("click", makeURLFilters)
 
-// add all search strings together
-let searchReady = searchStart.concat(filters).concat(APIKey)
 
 // fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=AJNDweW2mpoIQRcy43HbdJgdq")
 // fetch(`${searchReady}`)
