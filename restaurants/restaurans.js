0996a7e52da3ed39ad8f9df8519471fb395615f4
nathan sepulveda <nathans.composer@gmail.com>
2019-01-25 14:24:45 -0600

    added text field search

--- restaurants/restaurans.js
+++ restaurants/restaurans.js
@@ -1,58 +1,111 @@
 
-let arrayOfResturants = [
+// let arrayOfResturants = [
 
-]
+// ]
 
-foodType = prompt("What are you looking for?")
+// let foodType;
+// let clickAmountFood = 0
+// let textArea = document.getElementById("foodSearch");
+// let space = document.getElementById("putHere")
 
 
-fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}&sort=rating
+// let searcherFood = () => {
+//     // clickAmountFood += 1
+//     let foodType = textArea.value
+//     fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}&sort=rating
 
-`)
-    .then(response => response.json())
-    .then(restInfo => {
-        console.table(restInfo)
-        for (let i = 0; i < restInfo.restaurants.length; i++) {
-            let name = restInfo.restaurants[i].restaurant.name;
-            let type = restInfo.restaurants[i].restaurant.cuisines;
-            let cost = restInfo.restaurants[i].restaurant.average_cost_for_two;
-            let location = restInfo.restaurants[i].restaurant.location.locality
-            let rating = restInfo.restaurants[i].restaurant.user_rating.aggregate_rating
-            let blankRestObject = {}
+// `)
+//         .then(response => response.json())
+//         .then(restInfo => {
 
-            blankRestObject.names = name
-            blankRestObject.types = type
-            blankRestObject.costs = cost
-            blankRestObject.ratings = rating
-            blankRestObject.locations = location
+//             // console.table(restInfo)
 
-            arrayOfResturants.push(blankRestObject)
-        }
-        console.table(arrayOfResturants)
-        if (arrayOfResturants.length < 3) {
-            alert("Please try another query.")
-        }
-        arrayOfResturants.forEach(element => {
+//             for (let i = 0; i < restInfo.restaurants.length; i++) {
+//                 let name = restInfo.restaurants[i].restaurant.name;
+//                 let type = restInfo.restaurants[i].restaurant.cuisines;
+//                 let cost = restInfo.restaurants[i].restaurant.average_cost_for_two;
+//                 let location = restInfo.restaurants[i].restaurant.location.locality
+//                 let rating = restInfo.restaurants[i].restaurant.user_rating.aggregate_rating
+//                 let index = i
+//                 let blankRestObject = {}
 
-            let html = `
-                <h2>${element.names}</h2>
-                <h3>Type: ${element.types}</h3>
-                <p> Cost: $${element.costs} for two people  Rating: ${element.ratings}  Location: ${element.locations}</p>
-            `
-        document.getElementById("putHere"). innerHTML += html
-        
+
+//                 blankRestObject.indexes = index
+//                 blankRestObject.names = name
+//                 blankRestObject.types = type
+//                 blankRestObject.costs = cost
+//                 blankRestObject.ratings = rating
+//                 blankRestObject.locations = location
+
+//                 arrayOfResturants.push(blankRestObject)
+
+
+//             }
+//             if (arrayOfResturants.length < 3) {
+//                 alert("Please try another query.")
+//             }
+
+//             console.log(clickAmountFood)
+//             console.table(arrayOfResturants)
+//             arrayOfResturants.forEach(element => {
+//                 let html = `
+//                  <h2>${element.names}</h2>
+//                 <h3>Type: ${element.types}</h3>
+//                 <p> Cost: $${element.costs} for two people  Rating: ${element.ratings}  Location: ${element.locations}</p>
+//                 `
+//                 document.getElementById("putHere").innerHTML += html
+//             })
+//         })
+//     }
+
+
+    let arrayOfResturants = [
+
+    ]
+    
+    // foodType = prompt("What are you looking for?")
+    let textArea = document.getElementById("foodSearch");
+
+    let foodSearcher = () => {
+        let foodType = textArea.value
+        console.log(foodType)
+    
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}&sort=rating
+    `)
+        .then(response => response.json())
+        .then(restInfo => {
+            console.table(restInfo)
+            for (let i = 0; i < restInfo.restaurants.length; i++) {
+                let name = restInfo.restaurants[i].restaurant.name;
+                let type = restInfo.restaurants[i].restaurant.cuisines;
+                let cost = restInfo.restaurants[i].restaurant.average_cost_for_two;
+                let location = restInfo.restaurants[i].restaurant.location.locality
+                let rating = restInfo.restaurants[i].restaurant.user_rating.aggregate_rating
+                let blankRestObject = {}
+    
+                blankRestObject.names = name
+                blankRestObject.types = type
+                blankRestObject.costs = cost
+                blankRestObject.ratings = rating
+                blankRestObject.locations = location
+    
+                arrayOfResturants.push(blankRestObject)
+            }
+            console.table(arrayOfResturants)
+            if (arrayOfResturants.length < 3) {
+                alert("Please try another query.")
+            }
+            arrayOfResturants.forEach(element => {
+    
+                let html = `
+                    <h2>${element.names}</h2>
+                    <h3>Type: ${element.types}</h3>
+                    <p> Cost: $${element.costs} for two people  Rating: ${element.ratings}  Location: ${element.locations}</p>
+                `
+            document.getElementById("putHere"). innerHTML += html
+            
+            })
         })
-    })
+    }
 
-
-
-
-// arrayOfResturants.forEach(element => {
-
-//     let html = `
-//         <h1>${element.names}</h1>
-//         <h2>Type: ${element.types}</h2>
-//     `
-// document.getElementById("putHere"). innerHTML += html
-
-// })
+    document.getElementById("submitButton").addEventListener("click", foodSearcher)
