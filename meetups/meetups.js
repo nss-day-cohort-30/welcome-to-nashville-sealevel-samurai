da86bb13acb645e323d03a901a7f29451eeaad3a
jacob sanders <jkehsanders@gmail.com>
2019-01-25 13:53:20 -0600

    submit button works, addHtmlToDom works

--- meetups/meetups.js
+++ meetups/meetups.js
@@ -1,12 +1,32 @@
-fetch(`https://www.eventbriteapi.com/v3/events/search/?location.address=nashville&location.within=50mi&token=LSMF733OV2VR6EEC2LEY`, {
-    headers: {
-        "accept": "application/json"
-      },
-})
-    .then(response => response.json())
-    .then(entries => {console.log(entries)})
-    .then(
-    for (let i = 0; i < entries.length; i++) {
-        const element = entries[i];
-        console.table(element)
-    })
+let results = []
+const eventUrl ="https://www.eventbriteapi.com/v3/events/search/?location.address=nashville&location.within=50mi&categories="
+const eventToken ="&token=LSMF733OV2VR6EEC2LEY"
+let myUrlSearch = ""
+let makeUrl = eventUrl.concat(myUrlSearch).concat(eventToken) 
+let getSearchTerms = () => {
+   myUrlSearch = document.getElementById("searchTerms").value
+   console.log(myUrlSearch)
+   fetch(makeUrl)
+   .then(response => response.json)
+   .then(results.push(myUrlSearch))
+   .then(addHtmlToDom(myUrlSearch ))
+   
+}
+ console.log(results)
+
+ let htmlBuilder = (myUrlSearch) =>  {
+ return `
+
+ <p>${myUrlSearch.name}<p>
+ 
+ `}      
+ 
+ const addHtmlToDom = (htmlElement) => {
+     document.querySelector("#apiResults").innerHTML += htmlElement
+    }
+    
+    
+    
+
+ document.getElementById("input").addEventListener("click", getSearchTerms, addHtmlToDom)
+    
