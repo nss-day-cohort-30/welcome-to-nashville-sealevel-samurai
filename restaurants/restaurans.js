dc287d814074c00786a6188aba972f46ba572f3c
nathan sepulveda <nathans.composer@gmail.com>
2019-01-25 10:11:18 -0600

    added rating parameter

--- restaurants/restaurans.js
+++ restaurants/restaurans.js
@@ -6,7 +6,7 @@ let arrayOfResturants = [
 foodType = prompt("What are you looking for?")
 
 
-fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}
+fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&apikey=a5b9a73d046e9d6841fc6862a427d076&q=${foodType}&sort=rating
 
 `)
     .then(response => response.json())
@@ -17,11 +17,13 @@ fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=
             let type = restInfo.restaurants[i].restaurant.cuisines;
             let cost = restInfo.restaurants[i].restaurant.average_cost_for_two;
             let location = restInfo.restaurants[i].restaurant.location.locality
+            let rating = restInfo.restaurants[i].restaurant.user_rating.aggregate_rating
             let blankRestObject = {}
 
             blankRestObject.names = name
             blankRestObject.types = type
             blankRestObject.costs = cost
+            blankRestObject.ratings = rating
             blankRestObject.locations = location
 
             arrayOfResturants.push(blankRestObject)
