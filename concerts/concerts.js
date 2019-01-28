9b036d4e5610a6b62b727055728115fc29566b1d
Charles Woodring <charles.s.woodring@gmail.com>
2019-01-28 09:06:37 -0600

    dom working

--- concerts/concerts.js
+++ concerts/concerts.js
@@ -1,34 +1,34 @@
-let concerts = []
+// let events = []
+const listEl = document.getElementById("eventList")
+
 
 fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=312&apikey=1IOrDckSKm979mY3YBxYLuqcNhgSUD26")
 .then(response => response.json())
-    .then(concerts => {
-      //  console.table(concerts)
-       console.log(concerts._embedded.events[0].classification)
-      }
-      );
+.then(myParsedEvents => {
+  console.log(myParsedEvents._embedded)
+  for (let i = 0; i < myParsedEvents._embedded.events.length; i++) {
+    let artist = myParsedEvents._embedded.events[i].name
+    let venue = myParsedEvents._embedded.events[i]._embedded.venues[0].name
+    let date = myParsedEvents._embedded.events[i].dates.start.localDate
+    let time = myParsedEvents._embedded.events[i].dates.start.localTime
+    let genre = myParsedEvents._embedded.events[i].classifications[0].genre.name
+    let image = myParsedEvents._embedded.events[i].images[3].url
+    let eventCard = `
+    <h1>${artist}</h1>
+    <h2>${venue}</h2>
+    <p>${date} ${time}</p>
+    <p>${genre}</p>
+    <img src="${image}" class="image">
+    `
+    listEl.innerHTML+=eventCard
+    // console.log(artist)
+    // console.log(venue)
+    // console.log(date)
+    // console.log(time)
+    // console.log(genre)
+    // console.log(image)
+  }
+})
 
-
-// const genre = () => {
-//   return `
-//   <article class="musicGenre">
-//   <section>
-//       Genre: ${_embedded.}
-//   </section>
-//   </article>
-// }
-
-// fetch({
-//     type:"GET",
-//     url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=312&apikey=1IOrDckSKm979mY3YBxYLuqcNhgSUD26",
-//     async:true,
-//     dataType: "json",
-//     success: function(json) {
-//                 console.log(json);
-//                 // Parse the response.
-//                 // Do other things.
-//              },
-//     error: function(xhr, status, err) {
-//                 // This time, we do not end up here!
-//              }
-//   });
+// console.log(listEl)
+// const addEventToDom = artist => listEl.innerHTML += artist
